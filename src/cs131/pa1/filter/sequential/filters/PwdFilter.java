999da811d2a5fd6248b6387024d26775ae93f0a1
Julia Zhu <zzhu52813@gmail.com>
2018-09-24 17:29:03 -0400

    Added Pwd and Wc filters

--- src/cs131/pa1/filter/sequential/filters/PwdFilter.java
+++ src/cs131/pa1/filter/sequential/filters/PwdFilter.java
@@ -1,12 +1,34 @@
 package cs131.pa1.filter.sequential.filters;
 
+import java.util.LinkedList;
+
+import cs131.pa1.filter.Message;
 import cs131.pa1.filter.sequential.SequentialFilter;
+import cs131.pa1.filter.sequential.SequentialREPL;
 
 public class PwdFilter extends SequentialFilter{
+	private String[] components;
+	private String name;
+	
+	public PwdFilter(String subCommand) {
+		output = new LinkedList<>();
+		this.components = subCommand.split(" ");	
+		this.name = components[0];
+	}
+	
+	@Override
+	public void process() {
+		if(prev != null) {
+			System.out.print(Message.CANNOT_HAVE_INPUT.with_parameter(this.name));
+			return; 
+		}
+		
+		output.add(SequentialREPL.currentWorkingDirectory);
+	}
 
+	
 	@Override
 	protected String processLine(String line) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
--- src/cs131/pa1/filter/sequential/filters/WcFilter.java
+++ src/cs131/pa1/filter/sequential/filters/WcFilter.java
@@ -1,13 +1,42 @@
 package cs131.pa1.filter.sequential.filters;
 
+import java.util.LinkedList;
+
+import cs131.pa1.filter.Message;
 import cs131.pa1.filter.sequential.SequentialFilter;
 
 public class WcFilter extends SequentialFilter{
+	private int lines;
+	private int words;
+	private int characters; 
+	private String[] components;
 
+	public WcFilter(String subCommand) {
+		output = new LinkedList<>();
+		input = new LinkedList<>();
+		this.components = subCommand.split(" ");	
+		this.lines = 0;
+		this.words = 0;
+		this.characters = 0;
+	}
+	
+	@Override
+	public void process() {
+		if (input.isEmpty()) {
+			System.out.print(Message.REQUIRES_INPUT.with_parameter(this.components[0]));
+			return;
+		}
+		super.process();
+		output.add(this.lines + " " + this.words + " " + this.characters);
+	}
+	
 	@Override
 	protected String processLine(String line) {
-		// TODO Auto-generated method stub
+		this.lines++;
+		this.words += line.split(" ").length;
+		this.characters += line.length();
 		return null;
 	}
 
 }
+
