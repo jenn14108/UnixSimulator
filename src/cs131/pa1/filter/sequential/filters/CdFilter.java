1352ac35e55e52360c8acad8cbb7954033153f24
Julia Zhu <zzhu52813@gmail.com>
2018-09-25 15:30:39 -0400

    fixed merge conflict

--- src/cs131/pa1/filter/sequential/filters/CdFilter.java
+++ src/cs131/pa1/filter/sequential/filters/CdFilter.java
@@ -1,57 +1,55 @@
 package cs131.pa1.filter.sequential.filters;
 
-<<<<<<< HEAD
+
 import java.io.File;
 import java.util.LinkedList;
 
 import cs131.pa1.filter.*;
 import cs131.pa1.filter.sequential.*;
-=======
 import java.util.LinkedList;
 
 import cs131.pa1.filter.Filter;
 import cs131.pa1.filter.Message;
 import cs131.pa1.filter.sequential.SequentialFilter;
 import cs131.pa1.filter.sequential.SequentialREPL;
->>>>>>> origin/master
+
 
 public class CdFilter extends SequentialFilter {
 
 	private String[] components;
 	private String name;
-<<<<<<< HEAD
 	private String newDirc;
 	private String currDirc = SequentialREPL.currentWorkingDirectory;
-=======
+
 	private String directory;
->>>>>>> origin/master
-	
+
 	public CdFilter(String subCommand) {
 		this.components = subCommand.split(" ");	
 		this.name = components[0];
 	}
 	
+	@Override
 	public void process() {
-<<<<<<< HEAD
+
 		if(next != null) {
 			System.out.print(Message.CANNOT_HAVE_OUTPUT.with_parameter(name));
 			return; 
 		} else if (prev != null) {
 			System.out.print(Message.CANNOT_HAVE_INPUT.with_parameter(name));
 			return; 
-=======
+		}
+
 		if(prev != null) {
 			System.out.print(Message.CANNOT_HAVE_INPUT.with_parameter(name));
 			return; 
 		} else if (next != null) {
 			System.out.print(Message.CANNOT_HAVE_OUTPUT.with_parameter(name));
 			return; 
->>>>>>> origin/master
 		} else if (components.length < 2) {
 			System.out.print(Message.REQUIRES_PARAMETER.with_parameter(name));
 			return; 
 		}
-<<<<<<< HEAD
+
 		newDirc = components[1];
 		if (newDirc.equals(".")) {
 			return;
@@ -73,18 +71,8 @@ public class CdFilter extends SequentialFilter {
 			SequentialREPL.currentWorkingDirectory = dirc.getAbsolutePath();
 		}
 	}
-=======
-		directory = components[1];
-		if (directory.equals(".")) {
-			return;
-		} else if (directory.equals("..")) {
-			int firstSepr = SequentialREPL.currentWorkingDirectory.indexOf(Filter.FILE_SEPARATOR);
-			int lastSepr = SequentialREPL.currentWorkingDirectory.lastIndexOf(Filter.FILE_SEPARATOR);
-			
-		}
-	}
 	
->>>>>>> origin/master
+
 	
 	@Override
 	protected String processLine(String line) {
