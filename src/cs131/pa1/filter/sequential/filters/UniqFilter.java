a5888d3de5283ed8ff614f52f9e1c6e4e41be268
jenn14108 <jenn.lee14108@gmail.com>
2018-09-28 16:13:59 -0400

    Add comments for UNIQ

--- src/cs131/pa1/filter/sequential/filters/UniqFilter.java
+++ src/cs131/pa1/filter/sequential/filters/UniqFilter.java
@@ -19,6 +19,10 @@ public class UniqFilter extends ModifiedSequentialFilter{
 		components = commandAndParam.split(" ");
 	}
 	
+	/**
+	 * Process first checks whether there is input, which Uniq needs
+	 * if not, throw error. Otherwise, continue.
+	 */
 	@Override
 	public void process() {
 		
@@ -31,6 +35,10 @@ public class UniqFilter extends ModifiedSequentialFilter{
 		cont = true;
 	}
 	
+	/**
+	 * processLine uses a TreeSet to check whether a line 
+	 * in a file is unique. If it is, then add to output.
+	 */
 	@Override
 	protected String processLine(String line) {
 		if (checker.contains(line)) {
