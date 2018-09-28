8e51c277fd6c7fdc4f4de6286a710ed950396a00
Julia Zhu <zzhu52813@gmail.com>
2018-09-28 17:16:05 -0400

    comments for ModifiedSequentialFilter

--- src/cs131/pa1/filter/sequential/ModifiedSequentialFilter.java
+++ src/cs131/pa1/filter/sequential/ModifiedSequentialFilter.java
@@ -1,11 +1,14 @@
 package cs131.pa1.filter.sequential;
 
-
+/**
+ * This abstract class extends the SequentialFilter class and adds new fields necessary for 
+ * implementing Filters needed for the assignment
+ */
 public abstract class ModifiedSequentialFilter extends SequentialFilter{
-	protected String[] components;
-	protected boolean cont;
-	protected boolean contForCat = true;
-	protected String subCommand;
+	protected String[] components; //array that stores the split sub command
+	protected boolean cont; //determines whether to continue if the previous Filter prints an error
+	protected boolean contForCat = true; //determines whether to continue of Cat Filter throws an error
+	protected String subCommand; //stores the sub command String of the Filter
 	
 	@Override
 	protected String processLine(String line) {
