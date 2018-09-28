533f5d820ad2a64b374cc00d91e464ccf0e4fa1b
Julia Zhu <zzhu52813@gmail.com>
2018-09-28 16:30:49 -0400

    comments for Cd

--- src/cs131/pa1/filter/sequential/filters/CdFilter.java
+++ src/cs131/pa1/filter/sequential/filters/CdFilter.java
@@ -15,8 +15,8 @@ import cs131.pa1.filter.sequential.SequentialREPL;
 
 
 public class CdFilter extends ModifiedSequentialFilter {
-
-	private String newDirc;
+	
+	private String newDirc; //stores String of new directory
 	private String currDirc = SequentialREPL.currentWorkingDirectory;
 	
 	public CdFilter(String subCommand) {
@@ -27,27 +27,29 @@ public class CdFilter extends ModifiedSequentialFilter {
 	
 	@Override
 	public void process() {
-
-		if(next != null) {
+		
+		if(next != null) { //prints error when there's next filter
 			System.out.print(Message.CANNOT_HAVE_OUTPUT.with_parameter(subCommand));
 			return; 
-		} else if (prev != null) {
+		} else if (prev != null) { //prints error when there's a previous filter
 			System.out.print(Message.CANNOT_HAVE_INPUT.with_parameter(subCommand));
 			return; 
-		} else if (components.length < 2) {
+		} else if (components.length < 2) { //prints error when there's no parameter
 			System.out.print(Message.REQUIRES_PARAMETER.with_parameter(subCommand));
 			return; 
 		}
-
-		newDirc = components[1].trim();
-		if (newDirc.equals(".")) {
+		
+		newDirc = components[1].trim(); 
+		if (newDirc.equals(".")) { //continue when new directory is the current directory
 			cont = true;
 			return;
-		} else if (newDirc.equals("..")) {
-			int firstSepr = currDirc.indexOf(Filter.FILE_SEPARATOR);
-			int lastSepr = currDirc.lastIndexOf(Filter.FILE_SEPARATOR);
+		} else if (newDirc.equals("..")) { //goes back to the last directory
+			int firstSepr = currDirc.indexOf(Filter.FILE_SEPARATOR); //index of first separator
+			int lastSepr = currDirc.lastIndexOf(Filter.FILE_SEPARATOR); //index of last file separator
 			
-			if(!currDirc.equals(Filter.FILE_SEPARATOR)){
+			//checks if we can go up from current directory
+			if(!currDirc.equals(Filter.FILE_SEPARATOR)){  
+				//sets new current working directory
 				SequentialREPL.currentWorkingDirectory = currDirc.substring(0, lastSepr); 
 			}
 			cont = true;
@@ -55,19 +57,19 @@ public class CdFilter extends ModifiedSequentialFilter {
 		}
 		
 		currDirc += Filter.FILE_SEPARATOR + newDirc; 
-		File dirc = new File(currDirc);
+		File dirc = new File(currDirc); //creates new directory from parameter
 		
+		//checks if the new directory exists
 		if (!dirc.isDirectory()) {
 			System.out.print(Message.DIRECTORY_NOT_FOUND.with_parameter(subCommand));
 			return;
 		}
+		
+		//sets new current working directory
 		SequentialREPL.currentWorkingDirectory = dirc.getAbsolutePath();
 		cont = true;
 	}
 	
-		
-
-
 	@Override
 	protected String processLine(String line) {
 		return null;
