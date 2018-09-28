49c0cbc79eda5c19da77d538657fbaa41883f333
jenn14108 <jenn.lee14108@gmail.com>
2018-09-28 16:20:28 -0400

    Add comments for REPL

--- src/cs131/pa1/filter/sequential/SequentialREPL.java
+++ src/cs131/pa1/filter/sequential/SequentialREPL.java
@@ -15,26 +15,27 @@ public class SequentialREPL {
 		
 		//obtain user commands
 		String commands = input.nextLine();
-		//System.out.println("This is the command: " + commands);
 		
 		//shell runs until user command equates to "exit" 
 		while (!commands.equals("exit")) {
 			if (!commands.equals("")) {
 				//Make a new sequential filter
 				List<ModifiedSequentialFilter> filters = SequentialCommandBuilder.createFiltersFromCommand(commands);
+				//If there is at least one command, process it first, then see whether an error is thrown, 
+				//If error was thrown from the last command, cease to continue onto the next filter(s). 
 				if (filters != null) {
 					filters.get(0).process();
 					for (int i = 1; i < filters.size();i++) {
 						if(filters.get(i-1).cont && filters.get(i-1).output.size() != 0) {
-							
 							filters.get(i).process();
-							
 						} else {
 							filters = null;
 							break;
 						}
 					}
 					
+					//Only print output if no errors were thrown when running filters, there is output to be printed,
+					//and CAT didn't throw an error
 					if(filters != null && filters.get(filters.size()-1).cont && filters.get(filters.size()-1).contForCat) {
 						Queue<String> outputs = filters.get(filters.size()-1).output;	
 						if (outputs != null) {
